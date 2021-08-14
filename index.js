6911d9108ca673d35d82cf9f35aa19922c960722
Missile Gurung <gurungmissile@gmail.com>
2021-08-14 20:15:23 +0530

    DrumKit website

---
+++ index.js
@@ -0,0 +1,57 @@
+//detecting button press
+var noOfDrumButtons = document.querySelectorAll("button").length
+for (i = 0; i < noOfDrumButtons; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function() {
+    var buttonInnerHTML = this.innerHTML
+    makesound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+//detecting key press
+document.addEventListener("keypress",function(event){
+  makesound(event.key)
+  buttonAnimation(event.key)
+});
+
+function makesound(key){
+  switch (key) {
+    case "w":
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+    case "k":
+      var kickBase = new Audio('sounds/kick-bass.mp3');
+      kickBase.play();
+      break;
+    case "l":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+    default:
+      console.log(buttonInnerHTML);
+  }
+}
+
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },100)
+}
